d719d93d4592990c007d0baf86a61ffd6e49cb15
HONG JUN, CHIU <91401043+james61124@users.noreply.github.com>
2023-02-07 17:51:25 +0800

    Time: 8 ms (93.06%), Space: 10.5 MB (77.29%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,49 +1,32 @@
 class Solution {
 public:
-    vector<int>LIS;
-    int recursive(vector<int>& nums, int index){
-        // cout<<index<<endl;
-        if(LIS[index]!=0){
-            return LIS[index];
-        }
-        int max_index = 0;
-        int max = 1;
-        if(index==0){
-            LIS[index] = 1;
-            return 1;
-        }else{
-            for(int i=0;i<index;i++){
-                int ans = recursive(nums,i);
-                // cout<<index<<" "<<i<<" "<<ans<<endl;
-                if(ans >= max && nums[i] < nums[index]){
-                    max = ans;
-                    max_index = i;
-                }
+    
+    int binary_search(vector<int> &nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while(right >= left){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] > target){
+                right = mid - 1;
+            }else if(nums[mid] < target){
+                left = mid + 1;
+            }else {
+                return mid;
             }
         }
-        if(nums[index] > nums[max_index]){
-            LIS[index] = max + 1;
-            return max + 1;
-        }else{
-            LIS[index] = max;
-            return max;
-        }
+        return left;
     }
     int lengthOfLIS(vector<int>& nums) {
-        LIS.resize(nums.size());
-        for(int i=0;i<LIS.size();i++){
-            LIS[i] = 0;
-        }
-        int ans = recursive(nums,nums.size()-1);
-        for(int i=0;i<LIS.size();i++){
-            cout<<LIS[i]<<" ";
-        }
-        int max = 0;
-        for(int i=0;i<LIS.size();i++){
-            if(LIS[i]>max){
-                max = LIS[i];
+        vector<int>LIS;
+        LIS.push_back(nums[0]);
+        for(auto num : nums){
+            if(num <= LIS[LIS.size()-1]){
+                int index = binary_search(LIS, num);
+                LIS[index] = num;
+            }else{
+                LIS.push_back(num);
             }
         }
-        return max;
+        return LIS.size();
     }
 };
