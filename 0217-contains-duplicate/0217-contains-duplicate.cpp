4b482f1332dabc9bb8807569dea338b3bfeed1ce
HONG JUN, CHIU <91401043+james61124@users.noreply.github.com>
2023-02-08 16:00:58 +0800

    Time: 131 ms (55.30%), Space: 69.5 MB (20.45%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int,int>visit;
+        unordered_map<int,int>visit;
         for(int i=0;i<nums.size();i++){
             if(visit[nums[i]]==1) return true;
             visit[nums[i]] = 1;
