040f852e3a82484ed17bfbe42edce4757790139d
HONG JUN, CHIU <91401043+james61124@users.noreply.github.com>
2023-02-08 15:51:13 +0800

    Time: 147 ms (52.85%), Space: 93.4 MB (11.64%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        map<int,int>table;
+        int left = 0;
+        int right = 1;
         int max_profit = 0;
-        for(int i=0;i<prices.size();i++){
-            if(table.find(prices[i])!=table.end()){
-                table[prices[i]]++;
+        while(right<prices.size()){
+            if(prices[left] > prices[right]){
+                left = right;
+                right++;
             }else{
-                table[prices[i]] = 1;
+                int profit = prices[right] - prices[left];
+                if(profit>max_profit){
+                    max_profit = profit;
+                }
+                right++;
             }
-        }
-        // for(auto iter = table.begin();iter!=table.end();iter++){
-        //     cout<<*iter<<endl;
-        // }
-        for(int i=0;i<prices.size();i++){
-            // cout<<i<<endl;
-            int max_income = table.rbegin()->first;
-            int profit = max_income - prices[i];
-            if(profit>max_profit){
-                max_profit = profit;
-            }
-            if(table[prices[i]]==1){
-                table.erase(prices[i]);
-            }else{
-                table[prices[i]]--;
-            }
-            
-            
         }
         return max_profit;
     }
