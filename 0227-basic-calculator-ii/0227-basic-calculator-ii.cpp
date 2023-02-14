7b419f3a24b48d8e66eae3c90e3e3c1d9561ee14
HONG JUN, CHIU <91401043+james61124@users.noreply.github.com>
2023-02-14 14:53:53 +0800

    Time: 21 ms (75.84%), Space: 11.3 MB (57.14%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -1,30 +1,6 @@
 class Solution {
 public:
-    int calculate(string s) {
-        stack<int>mystack;
-        int tmp = 0;
-        char sign = '+';
-        for(int i=0;i<s.size();i++){
-            if(s[i]==' ') continue;
-            if(isdigit(s[i])){
-                tmp = tmp * 10 + (s[i] - '0');
-            }else{
-                if(sign=='+') mystack.push(tmp);
-                else if(sign=='-') mystack.push(-tmp);
-                else if(sign=='*'){
-                    tmp = tmp * mystack.top();
-                    mystack.pop();
-                    mystack.push(tmp);
-                }else{
-                    tmp = mystack.top() / tmp ;
-                    mystack.pop();
-                    mystack.push(tmp);
-                }
-                tmp = 0;
-                sign = s[i];
-                
-            }
-        }
+    void calculate(stack<int>&mystack, char sign, int tmp){
         if(sign=='+') mystack.push(tmp);
         else if(sign=='-') mystack.push(-tmp);
         else if(sign=='*'){
@@ -36,6 +12,24 @@ public:
             mystack.pop();
             mystack.push(tmp);
         }
+    }
+    
+    int calculate(string s) {
+        stack<int>mystack;
+        int tmp = 0;
+        char sign = '+';
+        for(int i=0;i<s.size();i++){
+            if(s[i]==' ') continue;
+            if(isdigit(s[i])){
+                tmp = tmp * 10 + (s[i] - '0');
+            }else{
+                calculate(mystack, sign, tmp);
+                tmp = 0;
+                sign = s[i];
+                
+            }
+        }
+        calculate(mystack, sign, tmp);
         int ans = 0;
         while(mystack.size()!=0){
             ans = ans + mystack.top();
