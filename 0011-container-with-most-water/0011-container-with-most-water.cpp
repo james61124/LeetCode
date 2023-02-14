cb36f7085ea60953a2a3e905206942055238e9e3
HONG JUN, CHIU <91401043+james61124@users.noreply.github.com>
2023-02-15 01:35:05 +0800

    Time: 100 ms (66.04%), Space: 59.1 MB (53.97%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        map<int,vector<int>>mp;
-        set<int>index;
-        for(int i=0;i<height.size();i++){
-            mp[height[i]].push_back(i);
-            index.insert(i);
-        }
-        
+        int left = 0;
+        int right = height.size()-1;
         int max_area = 0;
-        for(auto iter = mp.begin();iter!=mp.end();iter++){
-            for(int j=0;j<iter->second.size();j++){
-                int max_index, area;
-                auto index_iter_begin = index.begin();
-                auto index_iter_end = index.rbegin();
-                if(abs(*index_iter_begin-iter->second[j])>=abs(*index_iter_end-iter->second[j])){
-                    area = iter->first * abs(*index_iter_begin-iter->second[j]);
-                }else{
-                    area = iter->first * abs(*index_iter_end-iter->second[j]);
-                }
-                if(area>=max_area) max_area = area;
-                index.erase(iter->second[j]);
-            }
-            
+        while(left<right){
+            int area = min(height[left], height[right])* (right - left);
+            max_area = max(max_area, area);
+            if(height[left]< height[right])left++;
+            else right--;
         }
         return max_area;
     }
