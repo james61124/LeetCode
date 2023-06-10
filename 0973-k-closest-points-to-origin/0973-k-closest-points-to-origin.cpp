50b169bdb049092679ceba3e3ae390d118109444
HONG JUN, CHIU <91401043+james61124@users.noreply.github.com>
2023-06-11 00:20:36 +0800

    Time: 988 ms (7.87%), Space: 189.3 MB (5.06%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-    static bool cmp(pair<int, vector<int>>a, pair<int, vector<int>>b){
-        return a.first < b.first;
-    }
+    struct CustomCompare {
+        bool operator()(pair<int, vector<int>> a, pair<int, vector<int>> b) {
+            return a.first > b.first;
+        }
+    };
     
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         vector<pair<int, vector<int>>>mp;
+        priority_queue<pair<int, vector<int>>, vector<pair<int, vector<int>>>, CustomCompare> pq;
         vector<vector<int>>ans;
         for(auto point : points) {
             int dis = point[0] * point[0] + point[1] * point[1];
-            mp.push_back(make_pair(dis, point));
+            pq.push(make_pair(dis, point));
+            // mp.push_back(make_pair(dis, point));
         }
-        sort(mp.begin(), mp.end(), cmp);
+        // sort(mp.begin(), mp.end(), cmp);
         int count = 0;
-        for(auto child : mp){
+        // for(auto child : mp){
+        //     if(count>=k) break;
+        //     ans.push_back(vector<int>{child.second[0], child.second[1]});
+        //     count++;
+        // }
+        while (!pq.empty()) {
             if(count>=k) break;
-            ans.push_back(vector<int>{child.second[0], child.second[1]});
+            ans.push_back(pq.top().second);
+            pq.pop();
             count++;
         }
         return ans;
