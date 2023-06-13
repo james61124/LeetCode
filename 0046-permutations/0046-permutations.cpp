292cc1b22fc7241a78f03985aee46a2d22def02f
HONG JUN, CHIU <91401043+james61124@users.noreply.github.com>
2023-06-14 02:30:47 +0800

    Time: 0 ms (100.00%), Space: 7.8 MB (49.75%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,72 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
+    vector<vector<int>> permute(vector<int>& nums) {
+        permutate(nums, 0);
+        return permutations;
+    }
     
-    void dfs(vector<int>&nums, vector<int>visited, vector<int>cur){
-        if(cur.size()==nums.size()){
-            // vector<int>child;
-            // for(int i=0;i<cur.size();i++){
-            //     child.push_back(nums[cur[i]]);
-            // }
-            ans.push_back(cur);
+    void permutate(vector<int>& nums, int first){
+        if(nums.size() == first) {
+            permutations.emplace_back(nums);
             return;
         }
         
-        for(int i=0;i<nums.size();i++){
-            if(visited[i]==0){
-                cur.push_back(nums[i]);
-                visited[i] = 1;
-                dfs(nums, visited, cur);
-                cur.pop_back();
-                visited[i] = 0;
-            }
+        for(int i = first; i < nums.size(); ++i) {
+            swap(nums[i], nums[first]);
+            permutate(nums, first+1);
+            swap(nums[i], nums[first]);
         }
-//         for(int i=0;i<graph[index].second.size();i++){
-//             int graph_index = graph[index].second[i];
-//             // cout<<index<<" "<<graph_index<<endl;
-//             if(visited[graph_index]==0){
-//                 cur.push_back(graph_index);
-//                 visited[graph_index] = 1;
-//                 dfs(nums, graph, visited, cur);
-//                 cur.pop_back();
-//                 visited[graph_index] = 0;
-//             }
-//             // cur.pop_back();
-            
-//         }
     }
     
-    vector<vector<int>> permute(vector<int>& nums) {
-        // vector<pair<int, vector<int>>>graph;
-        // for(int i=0;i<nums.size();i++){
-        //     vector<int>child;
-        //     for(int j=0;j<nums.size();j++){
-        //         if(i!=j) child.push_back(j);
-        //     }
-        //     graph.push_back(make_pair(i, child));
-        // }
-        
-        // for(int i=0;i<graph.size();i++){
-        //     cout<<graph[i].first<<" ";
-        //     for(int j=0;j<graph[i].second.size();j++){
-        //         cout<<graph[i].second[j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        
-        
-        
-        for(int i=0;i<nums.size();i++){
-            vector<int>cur;
-            vector<int>visited(nums.size(), 0);
-            cur.push_back(nums[i]);
-            visited[i] = 1;
-            dfs(nums, visited, cur);
-        }
-        
-        
-        return ans;
-        
-        
-    }
+private:
+    vector<vector<int>> permutations;
 };
