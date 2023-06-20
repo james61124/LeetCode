607ce6109bb2f760c576c076d45e2b80608f17ab
HONG JUN, CHIU <91401043+james61124@users.noreply.github.com>
2023-06-20 23:56:56 +0800

    Time: 211 ms (12.84%), Space: 33.1 MB (87.61%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int find(vector<int>&all_account, int a){
+        int i = a;
+        while(true){
+            if(all_account[i]==i) return i;
+            else i = all_account[i];
+        }
+    }
+    
+    void change_root(vector<int>&all_account, int a, int b){
+        all_account[find(all_account, a)] = all_account[find(all_account, b)];
+    }
+    
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        vector<int>all_account(accounts.size());
+        map<string, int>mp;
+        vector<vector<string>>ans;
+        unordered_map<int, set<string>>table;
+        unordered_map<int, string>name_table;
+        for (int i = 0; i < accounts.size(); i++) {
+            all_account[i] = i;
+        }
+        for(int i=0;i<accounts.size();i++){
+            for(int j=1;j<accounts[i].size();j++){
+                if(mp.count(accounts[i][j])==0){
+                    mp[accounts[i][j]] = i;
+                }else{
+                    change_root(all_account, mp[accounts[i][j]], i);
+                }
+            }
+        }
+        for(int i=0;i<accounts.size();i++){
+            for(int j=1;j<accounts[i].size();j++){
+                int index = find(all_account, i);
+                name_table[index] = accounts[index][0];
+                table[index].insert(accounts[i][j]);
+                // cout<<index<<" "<<accounts[i][j]<<endl;
+            }
+        }
+//         for (const auto& kv : table) {
+//             const int& key = kv.first;
+//             const std::set<std::string>& values = kv.second;
+
+//             std::cout << "Key: " << key << std::endl;
+//             std::cout << "Values:";
+
+//             for (const auto& value : values) {
+//                 std::cout << " " << value;
+//             }
+
+//             std::cout << std::endl;
+//         }
+        for (const auto& it: table) {
+            vector<string>name;
+            const auto& values = it.second;
+            name.push_back(name_table[it.first]);
+            ans.push_back(name);
+            for (auto iter = it.second.cbegin(); iter != it.second.cend(); iter++) {
+                ans[ans.size()-1].push_back(*iter);
+            }
+        }
+        return ans;
+    }
+};
