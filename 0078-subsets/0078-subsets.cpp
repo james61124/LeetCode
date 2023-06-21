61ed14135d92f295d7b5f17757e3d92db62f6a7a
HONG JUN, CHIU <91401043+james61124@users.noreply.github.com>
2023-06-22 02:54:17 +0800

    Time: 6 ms (28.32%), Space: 7.5 MB (48.56%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
     vector<vector<int>>ans;
-    void dfs(vector<int>& nums, map<int, int>visit, vector<int>trajectory, int start){
+    void dfs(vector<int>& nums, vector<int>trajectory, int start){
         if(start==nums.size()){
             return;
         }
         for(int i=start; i<nums.size();i++){
             int num = nums[i];
-            // if(visit[num]==0){
-                // visit[num] = 1;
-                trajectory.push_back(num);
-                ans.push_back(trajectory);
-                dfs(nums, visit, trajectory, i+1);
-                // visit[num] = 0;
-                trajectory.pop_back();
-            // }
+            trajectory.push_back(num);
+            ans.push_back(trajectory);
+            dfs(nums, trajectory, i+1);
+            trajectory.pop_back();
         }
     }
     
     vector<vector<int>> subsets(vector<int>& nums) {
-        map<int, int>visit;
         vector<int>trajectory;
-        // for(auto num: nums){
-        //     visit[num] = 0;
-        // }
         ans.push_back(trajectory);
-        dfs(nums, visit, trajectory, 0);
+        dfs(nums, trajectory, 0);
         return ans;
     }
 };
