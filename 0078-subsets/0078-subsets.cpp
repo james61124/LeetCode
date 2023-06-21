55994f839b57d35aba272b6284ce7347fd746799
HONG JUN, CHIU <91401043+james61124@users.noreply.github.com>
2023-06-22 02:53:30 +0800

    Time: 6 ms (28.32%), Space: 7.3 MB (53.08%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -7,23 +7,23 @@ public:
         }
         for(int i=start; i<nums.size();i++){
             int num = nums[i];
-            if(visit[num]==0){
-                visit[num] = 1;
+            // if(visit[num]==0){
+                // visit[num] = 1;
                 trajectory.push_back(num);
                 ans.push_back(trajectory);
                 dfs(nums, visit, trajectory, i+1);
-                visit[num] = 0;
+                // visit[num] = 0;
                 trajectory.pop_back();
-            }
+            // }
         }
     }
     
     vector<vector<int>> subsets(vector<int>& nums) {
         map<int, int>visit;
         vector<int>trajectory;
-        for(auto num: nums){
-            visit[num] = 0;
-        }
+        // for(auto num: nums){
+        //     visit[num] = 0;
+        // }
         ans.push_back(trajectory);
         dfs(nums, visit, trajectory, 0);
         return ans;
