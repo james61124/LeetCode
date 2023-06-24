cd733bd9bc2bc63c630df6f37664ba8077ddbe7e
HONG JUN, CHIU <91401043+james61124@users.noreply.github.com>
2023-06-24 14:15:52 +0800

    Time: 27 ms (36.04%), Space: 8.9 MB (12.03%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        map<char, int>mp_magazine;
-        map<char, int>mp_ransomNote;
+        map<char, int>mp;
         for(auto s : magazine){
-            mp_magazine[s]++;
+            mp[s]++;
         }
         for(auto s : ransomNote){
-            mp_ransomNote[s]++;
-        }
-        for (auto it = mp_ransomNote.begin(); it != mp_ransomNote.end(); ++it) {
-            if(it->second>mp_magazine[it->first]){
-                return false;
-            }
+            mp[s]--;
+            if(mp[s]<0) return false;
         }
         return true;
-        
     }
 };
